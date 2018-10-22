311f4997e187fcc1dd3116d2687045c3b5fe4ece
Rebekah <rebekah_7567@hotmail.co.uk>
2018-10-22 15:36:57 +0100

    first commit

---
+++ classes_lab.rb
@@ -0,0 +1,33 @@
+class Student
+
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  
+  end
+
+  def name
+    return @name
+  end
+
+  def cohort
+    return @cohort
+  end
+
+  # def language
+  #   return @language
+  # end
+
+  def set_name(new_name)
+    return @name = new_name
+  end
+
+  def talk()
+    return "I can talk!"
+  end
+
+  def say_favourite_language(language)
+    return "I love #{language}"
+  end
+
+end
